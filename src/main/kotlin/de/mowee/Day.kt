212e65f65dbd990317616139a99642fac4939f42
Moritz Wörle <moritz@woerle.dev>
2022-12-19 17:35:49 +0100

    Better error message if file does not exist

--- src/main/kotlin/de/mowee/Day.kt
+++ src/main/kotlin/de/mowee/Day.kt
@@ -6,11 +6,16 @@ abstract class Day<T> {
 
     abstract val day: Int
 
+    private val filename: String
+        get() = lazy { "day$day.txt" }.value
+
     protected val input: String
-        get() = getResourceAsText("day${day}.txt")
+        get() = getResourceAsText(filename)
 
     private fun getResourceAsText(path: String): String {
-        return File(ClassLoader.getSystemResource(path).file).readText()
+        val systemResource = ClassLoader.getSystemResource(path)
+            ?: throw IllegalArgumentException("$filename does not exist!")
+        return File(systemResource.file).readText()
     }
 
     abstract fun partOne(): T
